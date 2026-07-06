bd969d8648445166b5c4e01c1a3a60297eac9b9e
MatheusBR0110 <matheus.fernandes01@escola.pr.gov.br>
2026-07-06 11:43:24 -0300

    Add files via upload

--- Avancando00/modelo.py
+++ Avancando00/modelo.py
@@ -3,47 +3,54 @@
 #Filme: Nome, ano, duração, curtir
 #Séries: Nome, ano, temporadas, curtir
 
-class Filmes:
-    def __init__(self, nome,ano,duracao):
-        self.__nome = nome.title()
+#Classe mãe/principal
+#Super Classe
+class Programas:
+    def __init__(self, nome,ano):
+        self._nome = nome.title()
         self.ano = ano
-        self.duracao = duracao
-        self.__curtir = 0
-
-    @property
-    def valor_curtir(self):
-        return self.__curtir
-
-    @property
-    def valor_nome(self):
-        return self.__nome
-
-    def curtida(self):
-        self.curtir += 1
-
-class Series:
-    def __init__(self, nome,ano,temporadas):
-        self.__nome = nome.title()
-        self.ano = ano
-        self.temporadas = temporadas
-        self.__curtir = 0
+        self._curtir = 0
     
     @property
     def valor_curtir(self):
-        return self.__curtir
+        return self._curtir
 
     @property
     def valor_nome(self):
-        return self.__nome
+        return self._nome
 
     def curtida(self):
-        self.curtir += 1
+        self._curtir += 1
 
+    @property
+    def valor_curtir(self):
+        return self._curtir
+
+    @property
+    def valor_nome(self):
+        return self._nome
+
+    def curtida(self):
+        self._curtir += 1
+
+class Filmes(Programas):
+    def __init__(self, nome,ano,duracao):
+        super().__init__(nome, ano)
+        self.duracao = duracao
+
+class Series(Programas):
+    def __init__(self, nome,ano,temporadas):
+        super().__init__(nome, ano)
+        self.temporadas = temporadas
 
 The_Boys = Series("The Boys", 2019, 5)
 The_Boys.curtida()
-print(f"Nome: {The_Boys.nome} - Ano: {The_Boys.ano} - Temporadas: {The_Boys.temporadas} - Curtidas: {The_Boys.curtir}")
 
 Rei_Leao= Filmes("Rei Leão", 1994, 94)
 Rei_Leao.curtida()
-print(f"Nome: {Rei_Leao.nome} - Ano: {Rei_Leao.ano} - Duração: {Rei_Leao.duracao} - Curtidas: {Rei_Leao.curtir}")
+
+filmes_series = [The_Boys, Rei_Leao]
+
+for programas in filmes_series:
+    detalhe = programas.duracao if hasattr(programas, 'duracao') else programas.temporadas
+    print(f"{programas._nome} - {programas.ano} - {detalhe}")
