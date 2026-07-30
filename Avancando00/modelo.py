4bba7d766cb1cf7693c2ef402288a8b0f3293b5c
MatheusBR0110 <matheus.fernandes01@escola.pr.gov.br>
2026-07-30 08:09:38 -0300

    Add files via upload

--- Avancando00/modelo.py
+++ Avancando00/modelo.py
@@ -33,24 +33,24 @@ class Programas:
     def curtida(self):
         self._curtir += 1
 
-    def imprime(self):
-        print(f"{self._nome} - {self.ano} - {self._curtir} Curtidas")
+    def __str__(self):
+        return f"{self._nome} - {self.ano} - {self._curtir} Curtidas"
 
 class Filmes(Programas):
     def __init__(self, nome,ano,duracao):
         super().__init__(nome, ano)
         self.duracao = duracao
 
-    def imprime(self):
-        print(f"{self._nome} - {self.ano} - {self.duracao} Minutos - {self._curtir} Curtidas")
+    def __str__(self):
+        return f"{self._nome} - {self.ano} - {self.duracao} Minutos - {self._curtir} Curtidas"
 
 class Series(Programas):
     def __init__(self, nome,ano,temporadas):
         super().__init__(nome, ano)
         self.temporadas = temporadas
 
-    def imprime(self):
-        print(f"{self._nome} - {self.ano} - {self.temporadas} - {self._curtir} Curtidas")
+    def __str__(self):
+        return f"{self._nome} - {self.ano} - {self.temporadas} - {self._curtir} Curtidas"
 
 The_Boys = Series("The Boys", 2019, 5)
 The_Boys.curtida()
@@ -61,4 +61,4 @@ A_Odisseia.curtida()
 filmes_series = [The_Boys, A_Odisseia]
 
 for programas in filmes_series:
-    programas.imprime()
+    print(programas)
